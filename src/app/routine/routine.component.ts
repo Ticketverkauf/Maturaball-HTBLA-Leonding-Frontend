46ecc66e1a389425c9b84bae470f31ea20fdff92
Eros Klein <e.klein@HTBLALeonding.onmicrosoft.com>
2024-10-15 15:42:10 +0200

    finish first-prototype of routine

--- src/app/routine/routine.component.ts
+++ src/app/routine/routine.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
+export type Routine = {
+  name: string,
+  time: Date
+}
 @Component({
   selector: 'app-routine',
   standalone: true,
@@ -8,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrl: './routine.component.css'
 })
 export class RoutineComponent {
+  @Input()
+  color: string = 'violet';
 
+  routines: Routine[] = [
+    {name: 'Morning Routine', time: new Date(new Date().setHours(18, 0, 0, 0))},
+    {name: 'Afternoon Routine', time: new Date(new Date().setHours(19, 0, 0, 0))},
+    {name: 'Evening Routine', time: new Date(new Date().setHours(20, 0, 0, 0))},
+    {name: 'KFC-Popout', time: new Date(new Date().setHours(21, 0, 0, 0))},
+    {name: 'Nettspend Show', time: new Date(new Date().setHours(22, 0, 0, 0))},
+    {name: 'Diddy Themed Afterparty', time: new Date(new Date().setHours(0, 0, 0, 0))}
+  ];
+  protected readonly window = window;
 }
