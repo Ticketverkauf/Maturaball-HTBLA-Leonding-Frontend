f127cfdcc49e06665416e5ccd56067c3ebc30559
Eros Klein <e.klein@HTBLALeonding.onmicrosoft.com>
2024-10-26 14:27:00 +0200

    actually make use of the action parameter provided by button component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, inject} from '@angular/core';
 import {ButtonComponent} from '../button/button.component';
+import {MatDialog} from '@angular/material/dialog';
+import {PopupComponent} from '../popup/popup.component';
+import { DialogService } from '../dialog.service';
 
 @Component({
   selector: 'app-nav',
@@ -13,6 +16,14 @@ import {ButtonComponent} from '../button/button.component';
 export class NavComponent {
   homepage :string = "https://www.htl-leonding.at/";
 
+  readonly dialogService = inject(DialogService);
+
   @Input()
   color : string = 'violet';
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    this.dialogService.openDialog(PopupComponent, '400px', '600px', true);
+  }
 }
